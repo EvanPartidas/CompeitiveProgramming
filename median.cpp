b77961bb607e098867d2c8358571a6edec99ce0e
Evan Partidas <evanpartidas@doralacademy-Inspiron-3650>
2019-09-23 10:11:45 -0400

    Problem Median

---
+++ median.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin>>N;
+    int size = 0;
+    int mid=0;
+    for(int i=0;i<N;i++){
+        char *c;
+        cin>>c;
+        int n;
+        cin>>n;
+        if(c[0]=='r'){
+            if(size%2==1){
+
+            }
+        }else{
+
+        }
+    }
+
+}
