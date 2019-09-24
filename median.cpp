8f48de9ddc9665d131057d8a7bea6fcba91c6151
Evan Partidas <Evanpartidas@gmail.com>
2019-09-23 21:06:46 -0400

    Update median.cpp

--- median.cpp
+++ median.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
-#include <set>
+#include <map>
 
 using namespace std;
+
+map<int> tree;
+int mid = 0;//Median Number (Floored)
+int mp = 0;//Median Pointer
+
+void inc(){
+
+}
+
+void dec(){
+    if(--mp<1){
+        iterator del = tree.find(mid);
+        iterator floor = del;
+        floor--;
+        tree.erase(del);
+        mp = *floor;
+        mid = floor;
+
+    }
+}
 
 int main(){
     int N;
     cin>>N;
-    int size = 0;
-    int mid=0;
     for(int i=0;i<N;i++){
-        char *c;
-        cin>>c;
-        int n;
-        cin>>n;
-        if(c[0]=='r'){
-            if(size%2==1){
-
+        char* q;
+        int num;
+        cin>>q>>num;
+        iterator it = tree.find(num);
+        if(q[0]=='r'){
+            if(it==tree.end()){
+                cout<<"Wrong!"<<endl;
+                continue;
+            }
+            int a = it.reference;
+            if(a==1)
+                tree.erase(it);
+            *it--;
+            if(num<mid){
+                dec();
             }
         }else{
 
         }
     }
+
 
 }
