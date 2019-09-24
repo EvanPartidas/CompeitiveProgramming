10688204751c215a5c390945aa64ec025cce5c13
Evan Partidas <evanpartidas@gmail.com>
2019-09-23 22:17:19 -0400

    Update median.cpp

--- median.cpp
+++ median.cpp
@@ -3,22 +3,28 @@
 
 using namespace std;
 
-map<int> tree;
+map<int,int> tree;
 int mid = 0;//Median Number (Floored)
 int mp = 0;//Median Pointer
-
+int size = 0;//Number of elements
 void inc(){
+    map<int,int>::iterator ceil = tree.find(mid);
+    if((++mp)>(*ceil).second){
+        ceil++;
+        pair<int,int> p = *ceil;
+        mp = 1;
+        mid = p.first;
+    }
 
 }
 
 void dec(){
-    if(--mp<1){
-        iterator del = tree.find(mid);
-        iterator floor = del;
+    if((--mp)<1){
+        map<int,int>::iterator floor = tree.find(mid);
         floor--;
-        tree.erase(del);
-        mp = *floor;
-        mid = floor;
+        pair<int,int> p = *floor;
+        mp = p.second;
+        mid = p.first;
 
     }
 }
@@ -26,23 +32,43 @@ void dec(){
 int main(){
     int N;
     cin>>N;
+    tree[1]=2;
+    tree[2]=5;
+    tree[3]=2;
+    mid = 2;
+    mp = 3;
+    size = 9;
     for(int i=0;i<N;i++){
         char* q;
         int num;
         cin>>q>>num;
-        iterator it = tree.find(num);
+        map<int,int>::iterator it = tree.find(num);
+        bool odd = size%2==1;
         if(q[0]=='r'){
             if(it==tree.end()){
                 cout<<"Wrong!"<<endl;
                 continue;
             }
-            int a = it.reference;
-            if(a==1)
+            (*it).second--;
+            size--;
+            int freq = (*it).second;
+            if(freq==0){
                 tree.erase(it);
-            *it--;
-            if(num<mid){
+            }
+            if(size<1){
+                cout<<"Wrong!"<<endl;
+                continue;
+            }
+            if((num<mid)&&!odd){
+                inc();
+            }
+            else if((num>mid)&&odd){
                 dec();
             }
+            else if((num==mid)&&odd){
+                dec();
+            }
+            //printf("Num: %d Freq:%d\n Mid: %d\n Mp: %d\n Size: %d\n",num,freq,mid,mp,size);
         }else{
 
         }
