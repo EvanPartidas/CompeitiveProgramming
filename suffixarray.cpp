696b6aceaa2d9f1b5c1dfb53c011ac5ca8d86bb3
Evan Partidas <evanpartidas@doralacademy-Inspiron-3650>
2019-10-02 16:31:28 -0400

    Update suffixarray.cpp

--- suffixarray.cpp
+++ suffixarray.cpp
@@ -1,39 +1,66 @@
-
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
 const int MAXN = 100000;
 
-int N,C[MAXN],TRA[MAXN],RA[MAXN],TSA[MAXN],SA[MAXN],STR[MAXN];
+int N,C[MAXN],TRA[MAXN],RA[MAXN],TSA[MAXN],SA[MAXN];
+char STR[MAXN];
 
-int countsort(int k,int size){
+void countsort(int k,int size){
     int i;
     memset(C,0,size);
-    for(i=0;i<N;i++)
-        C[i+k<N?RA[i+k]:0]++;
+    for(i=0;i<N;i++){
+        cout<<C[SA[i]+k<N?RA[SA[i]+k]:0]++<<endl;
+	}
+    
     for(i=1;i<size;i++)
         C[i]+=C[i-1];
     for(i=0;i<N;i++)
-        TSA[--C[i+k<N?i+k:0]] = SA[i];
+        TSA[--C[SA[i]+k<N?RA[SA[i]+k]:0]] = SA[i];
     for(i=0;i<N;i++)
         SA[i]=TSA[i];
+}
+
+void print(){
+	puts("Printing...");
+    for(int i=0;i<N;i++){
+		cout<<(STR+SA[i])<<endl;
+	}	
 }
 
 void initSA(){
     int i,j,k,r;
     for(i=0;i<N;i++)
         RA[i]=STR[i],SA[i]=i;
+    print();
     for(k=1;k<N;k<<=1){
-            countsort(k);//Reverses the array stably
-            countsort(0);//Stabilizes the array (and is also necessary)
+            countsort(0,300);//Stabilizes the array (and is also necessary)
+			print();
+			puts("Sorting again");
+            countsort(k,300);//Reverses the array stably
+			print();
             TRA[SA[0]]=r=0;
             for(i=1;i<N;i++){
                 if(N-SA[i]<k)
                     TRA[i]=0;
                 else
                     TRA[SA[i]]=
-                        (RA[SA[i]]==RA[SA[i-1]]&&RA[SA[i]+k]==RA[SA[i]+k])?r:++r;
+                        (RA[SA[i]]==RA[SA[i-1]] && RA[SA[i]+k]==RA[SA[i-1]+k])?r:++r;
 
             }
-            for (i = 0; i < n; i++)
+            for (i = 0; i < N; i++)
                 RA[i] = TRA[i];
-            if (RA[SA[n-1]] == n-1) break;
+            if (RA[SA[N-1]] == N-1) break;
     }
+    puts("Fin");
+}
+
+int main(){
+	cin>>N;
+	cin>>STR;
+	STR[N++]='$';
+	initSA();
+	print();
+	return 0;
 }
