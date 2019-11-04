8249d9b4cd609d7f615c6b2b06536d81f2441b1a
Evan Partidas <evanpartidas@doralacademy-Inspiron-3650>
2019-11-04 16:27:31 -0500

    Update: MLE

--- almostunionfind.cpp
+++ almostunionfind.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
-#include <vector>
+#include <unordered_set>
 
 using namespace std;
 
 const int MAXN = 100005;
 
 int set[MAXN];
-int size[MAXN];
-int sum[MAXN];
-vector<int> children[MAXN];
+int count[MAXN];
+long long sum[MAXN];
+unordered_set<int> children[MAXN];
 
 
 int find(int node){
 	if(set[node]==node)
 		return node;
+	int parent = set[node];
 	set[node] = find(set[node]);
+	if(set[node]!=parent)
+		children[parent].erase(node);
 	return set[node];
 }
 
@@ -22,8 +25,36 @@ void merge(int a, int b){
 	a = find(a);
 	b = find(b);
 	set[b]=a;
-	size[a]+=size[b];
+	children[a].insert(b);
+	count[a]+=count[b];
 	sum[a]+=sum[b];
+}
+
+void remove(int node){
+	int root = find(node);
+	if(root==node){
+		bool first= true;
+		for(int child:children[node]){
+			if(first){
+				set[node]=child;
+				root = child;
+				count[root]= count[node]-1;
+				sum[root]=sum[node]-node;
+				first = false;
+			}
+			find(child);
+		}
+	}
+	else{
+		for(int child:children[node]){
+			find(child);
+		}
+		count[root]--;
+		sum[root]-= node;
+	}
+	count[node]= 1;
+	sum[node]= node;
+	set[node]= node;
 }
 
 int N,M;
@@ -31,22 +62,34 @@ int main(){
 
 	while(!cin.eof()){
 		cin>>N>>M;
+		for(int i=0;i<=N;i++){
+			count[i]=1;
+			set[i]=i;
+			sum[i]=i;
+			children[i].clear();
+		}
 		int type,p,q;
-		cin>>type>>p;
-		switch(type){
-			case 1:{
-				cin>>q;
-				merge(p,q);
-				break;
-			}
-			case 2:{
-				cin>>q;
-				merge(p,q);
-				break;
-			}
-			case 3:{
-				printf("%d %d\n",size[p],sum[p]);
-				break;
+		while(M-->0){
+			cin>>type>>p;
+			switch(type){
+				case 1:{
+					cin>>q;
+					merge(p,q);
+					break;
+				}
+				case 2:{
+					cin>>q;
+					if(find(p)==find(q))
+						break;
+					remove(p);
+					merge(p,q);
+					break;
+				}
+				case 3:{
+					p = find(p);
+					cout<<count[p]<<" "<<sum[p]<<endl;
+					break;
+				}
 			}
 		}
 	}
