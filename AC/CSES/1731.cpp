2881d083478e4a512cd44eddf7be78ddc2528e26
Evan Partidas <Evanpartidas@gmail.com>
2020-01-12 18:12:42 -0500

    Solved Word Combinations (H*ck yeah)

--- AC/CSES/1731.cpp
+++ AC/CSES/1731.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <fstream>
 
 using namespace std;
 typedef long long ll;
@@ -8,20 +7,20 @@ const int MAXN = 5005;
 const ll MOD = 1e9+7;
 
 string pattern;
-ll count[MAXN];
+ll count[MAXN];//This will serve as a dp array, accumulating results
 
-struct node{
-	node* ls[27];
+struct node{//Node structure for my minimalist trie
+	node* ls[27];//Array slot 26 is a terminator marker
 };
 
-node* root = new node();
+node* root = new node();//Root node for my suffix trie
 int N,K;
 
-void insert(string str){
+void insert(string str){//Insertion into trie
 	node* n = root;
 	int ind =0;
 	for(char letter:str){
-		if(ind++>MAXN)
+		if(ind++>MAXN)//Saving space (and time)
 			break;
 		letter-='a';
 		if(!(n->ls[letter])){
@@ -29,17 +28,25 @@ void insert(string str){
 		}
 		n = n->ls[letter];
 	}
-	n->ls[26] = root; 
+	n->ls[26] = root;//Set terminator equal to root (No reason other than saving space)
 }
 
-ll search(){
-	for(int start=0;start<N;start++){
+/**
+One way to visualize this is: for every letter i in the main word
+we want to figure out every (further) spot in the word you can
+get to. First we start with i as 0 and then go up to the last letter
+in the word, this pattern means that we will have every i before us 
+(and our current spot) computed already. We can simply keep track of 
+how many ways there are to get to any index as we go.
+*/
+ll search(){//O(N^2)
+	for(int start=0;start<N;start++){//Run for every suffix of the main word.
 		int ind = 0;
 		
 		for(node* n = root;start+ind<N&&n->ls[pattern[start+ind]-'a'];ind++){
 			n = n->ls[pattern[start+ind]-'a'];
-			if(n->ls[26]){
-				count[start+ind+1]+=count[start];
+			if(n->ls[26]){//Check if there is a terminator at this location
+				count[start+ind+1]+=count[start];//Add in this path
 				count[start+ind+1]%=MOD;
 			}
 		}		
@@ -47,16 +54,16 @@ ll search(){
 	return count[N];
 } 
 
-int main(){
+int main(){//Total: O(N^2+K)
 	cin>>pattern;
 	N = pattern.length();
 	cin>>K;
-	for(int i=0;i<K;i++){
+	for(int i=0;i<K;i++){//This is O(K)
 		string str;
 		cin>>str;
-		insert(str);
+		insert(str);//Insert every word into the suffix trie
 	}
 	count[0] = 1;
-	cout<<search();	
+	cout<<search();
 	return 0;
 }
