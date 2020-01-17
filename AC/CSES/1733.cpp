795d4800bc6760b6d7f669c0b52177f0803e2413
Evan Partidas <evanpartidas@doralacademy-Inspiron-3650>
2020-01-17 09:36:24 -0500

    Solved "Finding Periods"

---
+++ AC/CSES/1733.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <bitset>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+ 
+const ll mods[] = {0x7fffffff,(ll)(1e9+7)};
+ 
+ 
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	string str;
+	cin>>str;
+	ll hashfront[] = {0,0};
+	ll hashback[] = {0,0};
+	ll pows[] = {1,1};
+	vector<int> lens;
+	for(int i=0;i<str.length()-1;i++){
+		bool epic = true;
+		for(int j=0;j<2;j++){
+			hashfront[j] += (str[i]-'a'+1)*pows[j];
+			hashfront[j] %= mods[j];
+			hashback[j] *= 27;
+			hashback[j] %=mods[j];
+			hashback[j] += str[str.length()-i-1]-'a'+1;
+			hashback[j] %= mods[j];
+			epic&=hashfront[j]==hashback[j];
+			pows[j]*=27;
+			pows[j]%=mods[j];
+		}
+		if(epic)
+			lens.push_back(str.length()-i-1);
+	}
+	lens.push_back(str.length());
+	sort(lens.begin(),lens.end());
+	for(auto lol:lens)
+		cout<<lol<<" ";
+	return 0;
+}
