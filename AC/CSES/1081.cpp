5626b941bc20eac05591fe81e848001ee48b9602
BuildTools <unconfigured@null.spigotmc.org>
2020-01-27 15:21:12 -0500

    Solved "Counting Divisors" and "Common Divisors"

--- AC/CSES/1081.cpp
+++ AC/CSES/1081.cpp
@@ -1,41 +1,60 @@
 #include <iostream>
 #include <map>
 #include <math.h>
+#include <set>
+#include <vector>
 using namespace std;
 typedef unsigned long long ll;
-
+ 
 const int MAXX= 1e6+5;
 const int MAXN = 2e5+5;
-
+ 
 int SPF[MAXX];
-int ticks[MAXX];
-bool v[MAXX];
-
-
+bool seen[MAXX];
+set<int,greater<int>> factors;
+vector<int> toins;
+ 
 int main(){
-	for(int i=2;i<MAXX;i+=2) SPF[i]=2;
-	for (ll i = 3; i < MAXX; i += 2){
-		if (!v[i]){
-			SPF[i] = i;
-			for (ll j = i; (j*i) < MAXX; j += 2){
-				if (!v[j*i]){
-					v[j*i] = true, SPF[j*i] = i;
-				}
-			}
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	for(int j=2;j<=MAXX;j++){
+		if(SPF[j])
+			continue;
+		for (int i = j*2; i <=MAXX; i += j){
+			SPF[i] = j;
 		}
+		SPF[j]=j;
 	}
 	int N;
 	cin>>N;
 	int gcd=1;
-	while(N-->0){// O(N)
+	while(N-->0){
 		int X;
 		cin>>X;
-		while(X>1){//O(log X)
-			ticks[SPF[X]]++;
-			if(ticks[SPF[X]]>1)
-				gcd = max(gcd,SPF[X]);
+		if(X<gcd)
+			continue;
+		if(seen[X]){
+			gcd = max(gcd,X);
+			continue;
+		}
+		factors.clear();
+		factors.insert(1);
+		while(X>1){
+			toins.clear();
+			for(auto it = factors.begin();it!=factors.end();it++)
+				toins.push_back(SPF[X]*(*it));
+			factors.insert(toins.begin(),toins.end());
 			X/=SPF[X];
 		}
+		for(int i:factors){
+			if(i<=gcd) break;
+			if(seen[i]){
+				gcd = max(gcd,i);
+				break;
+			}
+			seen[i]=1;
+		}
+		seen[X]=1;
 	}
 	cout<<gcd<<endl;
 	return 0;
--- AC/CSES/1713.cpp
+++ AC/CSES/1713.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <map>
+#include <math.h>
 using namespace std;
-typedef long long ll;
+typedef unsigned long long ll;
 
 const int MAXX= 1e6+5;
 
@@ -11,11 +12,13 @@ bool v[MAXX];
 
 int main(){
 	for(int i=2;i<MAXX;i+=2) SPF[i]=2;
-	for (int i = 3; i < MAXX; i += 2){
+	for (ll i = 3; i < MAXX; i += 2){
 		if (!v[i]){
 			SPF[i] = i;
-			for (int j = i; (j*i) < MAXX; j += 2){
-				if (!v[j*i])	v[j*i] = true, SPF[j*i] = i;
+			for (ll j = i; (j*i) < MAXX; j += 2){
+				if (!v[j*i]){
+					v[j*i] = true, SPF[j*i] = i;
+				}
 			}
 		}
 	}
@@ -33,6 +36,11 @@ int main(){
 				(*it).second++;
 			X/=SPF[X];
 		}
+		ll num = 1;
+		for(auto it=tree.begin();it!=tree.end();it++){
+			num *= (*it).second;
+		}
+		cout<<num<<endl;
 	}
 	return 0;
 }
